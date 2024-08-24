cddc81037d610c2714031aa4896713da711a27a8
Saverio Cecere <s.cecere@be-tse.it>
2024-08-24 11:57:35 +0200

    RevertTest - primo commit

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace AOC
     {
         static void Main(string[] args)
         {
+            var reverttest = "primo commit";
             Console.WriteLine("Choose the option:");
             Console.WriteLine("1 - DataStructures tests:");
             Console.WriteLine("2 - Search Alghoritms:");
